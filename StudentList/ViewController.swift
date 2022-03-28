c2a9d771aba5f1261df60ed8c55595cab01fded6
Chris Bond <bondcb@bc.edu>
2022-03-28 16:41:16 -0400

    Initial Commit

---
+++ StudentList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentList
+//
+//  Created by Chris Bond on 3/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
